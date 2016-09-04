7f9055d656e346d191a555ef48ee31a43827248f
maprohu <maprohu@gmail.com>
2016-09-04 19:13:05 +0100

    voice

--- akka/stream/src/main/scala/toolbox8/akka/stream/Streams.scala
+++ akka/stream/src/main/scala/toolbox8/akka/stream/Streams.scala
@@ -1,9 +1,10 @@
 package toolbox8.akka.stream
 
 import akka.stream.scaladsl.Flow
+import akka.util.ByteString
 
 import scala.collection.immutable._
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Created by martonpapp on 31/08/16.
@@ -17,6 +18,8 @@ object Streams {
   def statefulMapAsyncConcat[State, In, Out](
     initial: State,
     fn: (State, In) => Future[(State, Out)]
+  )(implicit
+    executionContext: ExecutionContext
   ) = {
     Flow[In]
       .statefulMapConcat({ () =>
@@ -36,6 +39,8 @@ object Streams {
 
   def stateMachineMapAsyncConcat[In, Out](
     initial: State[In, Out]
+  )(implicit
+    executionContext: ExecutionContext
   ) = {
     statefulMapAsyncConcat[State[In, Out], In, Out](
       initial,
@@ -48,6 +53,28 @@ object Streams {
     def apply(in: In) : Future[(State[In, Out], Out)]
 
   }
+
+  def groupBytes(groupSize: Int) =
+    Flow[ByteString]
+      .statefulMapConcat({ () =>
+        var buffer = ByteString()
+
+        { bs =>
+          val b = buffer ++ bs
+
+          def helper(acc: List[ByteString], from: Int, until: Int) : (Seq[ByteString], ByteString) = {
+            if (b.length < until) (acc.reverse, b.slice(from, b.length))
+            else helper(b.slice(from, until) +: acc, until, until + groupSize)
+          }
+
+          val (groups, newBuffer) = helper(List.empty, 0, groupSize)
+
+          buffer = newBuffer
+
+          groups
+        }
+      })
+
 
 }
 
